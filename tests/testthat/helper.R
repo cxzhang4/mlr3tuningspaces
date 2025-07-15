4b7bdfeda82b907ea3983b33372123a7bc329247
Carson Zhang <carsonzhang4@gmail.com>
2025-07-15 11:53:32 +0200

    uncommented line in helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -5,7 +5,7 @@ library(mlr3learners)
 library(paradox)
 library(R6)
 
-# lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
 # nolint end
 
 test_tuning_space = function(key, learner) {
